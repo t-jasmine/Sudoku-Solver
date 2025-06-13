c2c406ce1e6f6a1abe9fab2989bef617a85f127c
Jasmine <jasminetran227@gmail.com>
2025-06-13 16:06:17 -0700

    Tweaked UI; Changed Fonts & TextField Appearance

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 
@@ -22,8 +23,19 @@ public class App extends Application {
 
     TextField[][] textFields = new TextField[9][9];
 
+    public void clearTextField(TextField t)
+    {
+        t.setText("");
+        t.setStyle("-fx-background-color:rgb(255, 255, 255); -fx-background-radius:0; -fx-border-width: 1; -fx-border-color: rgb(180, 165, 180);");
+    }
+
     @Override
     public void start(Stage stage) {
+
+        //Fonts & Customization
+        Font textFont = Font.font("Helvetica");
+        Font headerFont = Font.font("Helvetica", FontWeight.BOLD, 16);
+
         //9x9 Grid
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -38,14 +50,14 @@ public class App extends Application {
                 TextField t = new TextField();
                 t.setAlignment(Pos.CENTER);
                 textFields[c][r] = t; //not [r][c]  because columns represent x axis, rows are y-axis
-                textFields[c][r].setStyle("-fx-background-color:rgb(255,255,255)");
+                clearTextField(textFields[c][r]);
                 grid.add(t,c,r);
             }
         }
 
         //Sudoku Text Label
         Label textLabel = new Label("Sudoku Solver");
-        textLabel.setFont(new Font("Roboto",16));
+        textLabel.setFont(headerFont);
         HBox hbBox = new HBox(10);
         hbBox.getChildren().add(textLabel);
         hbBox.setAlignment(Pos.BOTTOM_LEFT);
@@ -53,6 +65,7 @@ public class App extends Application {
 
         //Clear Button
         Button clearBtn = new Button("Clear");
+        clearBtn.setFont(textFont);
         hbBox = new HBox(10);
         hbBox.getChildren().add(clearBtn);
         hbBox.setAlignment(Pos.BOTTOM_LEFT);
@@ -63,14 +76,14 @@ public class App extends Application {
             {
                 for(int c = 0; c<9; c++)
                 {
-                    textFields[c][r].setText("");
-                    textFields[c][r].setStyle("-fx-background-color:rgb(255,255,255)");
+                    clearTextField(textFields[c][r]);
                 } 
             }
         });
 
         //Solve Button
         Button solveBtn = new Button("Solve");
+        solveBtn.setFont(textFont);
         hbBox = new HBox(10);
         hbBox.getChildren().add(solveBtn);
         hbBox.setAlignment(Pos.BOTTOM_RIGHT);
@@ -83,7 +96,7 @@ public class App extends Application {
                 {
                     //testing, temp values, will add functionality later :3
                     textFields[c][r].setText("");
-                    textFields[c][r].setStyle("-fx-background-color:rgb(144, 209, 128)");
+                    textFields[c][r].setStyle("-fx-background-color:rgb(150, 235, 100); -fx-background-radius:0; -fx-border-width: 1; -fx-border-color: rgb(60, 185, 60);");
                 }
             }
         });
